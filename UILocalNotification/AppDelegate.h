5baaa90b50a4dd4df1931136ae1fa5b17dcfbbee
yi <zhengyi@tinckay.com>
2016-03-01 14:02:35 +0800

    Initial Commit

---
+++ UILocalNotification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UILocalNotification
+//
+//  Created by yi on 16/3/1.
+//  Copyright © 2016年 yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UILocalNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UILocalNotification
+//
+//  Created by yi on 16/3/1.
+//  Copyright © 2016年 yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
